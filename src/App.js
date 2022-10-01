ddf3d6d2b9758c21c20a6bb8d04991ad77cc8b0d
Pratham Gupta <prathamgupta.pg1@gmail.com>
2022-10-02 02:02:46 +0530

    Installed MaterialUI and added background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        minHeight: "100vh",
+        backgroundImage: `url(${
+            process.env.PUBLIC_URL + "/assets/background.jpg"
+        })`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+    },
+}));
+
 function App() {
-    return <div className="App">Test</div>;
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+        </div>
+    );
 }
 
 export default App;
