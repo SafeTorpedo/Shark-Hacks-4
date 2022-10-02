88a5e252f78ab9e87a3f7db9924297845812aab2
Pratham Gupta <prathamgupta.pg1@gmail.com>
2022-10-02 06:17:51 +0530

    Set up routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { CssBaseline } from "@material-ui/core";
 import Header from "./components/Header";
 import CardLinks from "./components/CardLinks";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,9 +21,25 @@ function App() {
     const classes = useStyles();
     return (
         <div className={classes.root}>
-            <CssBaseline />
-            <Header />
-            <CardLinks />
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <CssBaseline />
+                                <Header />
+                                <CardLinks />
+                            </>
+                        }
+                    ></Route>
+                    <Route
+                        path="/chat/:roomName"
+                        element={<h1>Test</h1>}
+                    ></Route>
+                    <Route path="*" element={<h1>404 Not Found</h1>}></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
