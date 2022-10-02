a3fcd040efe04358b22688e07d251cb4577df06f
Edward Yim <youngyu15@gmail.com>
2022-10-02 19:43:58 +0900

    telegram bot changes

--- src/static/features.js
+++ src/static/features.js
@@ -1,9 +1,9 @@
 const features = [
     {
-        title: "Sharky Filter",
-        desc: "Convert a picture into a sharky theme",
-        imageURL: process.env.PUBLIC_URL + "/assets/photo.jpg",
-        time: 1500,
+        title: "Telegram Bot",
+        desc: "Get quized and learn about shark facts",
+        imageURL: process.env.PUBLIC_URL + "/assets/telegram.png",
+        time: 1500
     },
     {
         title: "Sharky Chat ",
